1bbb2f71b784147a0dac39f5ec14ce37d36d953d
5enQ <11675502+5enQ@users.noreply.github.com>
2021-01-18 16:26:53 +0300

    Db Connection fix

---
+++ Cours/WebApplication1/Domain/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using WebApplication1.Domain.Repositories.Abstract;
+
+namespace MyCompany.Domain
+{
+    public class DataManager
+    {
+        public ITextFieldsRepository TextFields { get; set; }
+        public IServiceItemsRepository ServiceItems { get; set; }
+
+        public DataManager(ITextFieldsRepository textFieldsRepository, IServiceItemsRepository serviceItemsRepository)
+        {
+            TextFields = textFieldsRepository;
+            ServiceItems = serviceItemsRepository;
+        }
+    }
+}
--- Cours/WebApplication1/Startup.cs
+++ Cours/WebApplication1/Startup.cs
@@ -1,14 +1,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyCompany.Domain;
+using MyCompany.Domain.Repositories.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using WebApplication1.Domain;
+using WebApplication1.Domain.Repositories.Abstract;
 using WebApplication1.Service;
 
 namespace WebApplication1
@@ -18,26 +23,53 @@ namespace WebApplication1
         public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration) => Configuration = configuration;
 
-        //public Startup(IConfiguration configuration)
-        // {
-        //    Configuration = configuration;
-        //}
-
-        //public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /*public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        This method gets called by the runtime. Use this method to add services to the container.*/
         public void ConfigureServices(IServiceCollection services)
         {
             //подключаем конфиг из appsettings.json
             Configuration.Bind("Project", new Config());
 
-            //services.AddRazorPages();
+            //подключаем нужный функционал приложения в качестве сервисов
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            //подключаем контекст БД
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            //настраиваем identity систему
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            //настраиваем authentication cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "myCompanyAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
+            //добавляем сервисы для контроллеров и представлений (MVC)
             services.AddControllersWithViews()
                 .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        /*public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             //позволяет при разработке подучать полную информацию об ошибках
             if (env.IsDevelopment())
@@ -60,6 +92,33 @@ namespace WebApplication1
                 //регестрируем нужные эндпоинты
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }*/
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //!!! порядок регистрации middleware очень важен
+
+            //в процессе разработки нам важно видеть какие именно ошибки
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            //подключаем поддержку статичных файлов в приложении (css, js и т.д.)
+            app.UseStaticFiles();
+
+            //подключаем систему маршрутизации
+            app.UseRouting();
+
+            //подключаем аутентификацию и авторизацию
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            //регистриуруем нужные нам маршруты (ендпоинты)
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("admin", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
